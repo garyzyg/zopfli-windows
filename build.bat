38eb407a3f33934a46fc4cacab037a97854ad69b
Nick Shaforostov <mshaforostov@airmusictech.com>
2018-06-22 19:54:47 +0200

    simplify bat file to make it work on my win8.1 vs2015 system
    now you need to run it from "VS20xx <arch> Native Tools Command Prompt"

--- build.bat
+++ build.bat
@@ -1,9 +1,4 @@
-PUSHD .
-FOR %%I IN (C:\WinDDK\7600.16385.1) DO CALL %%I\bin\setenv.bat %%I fre %Platform% WIN7 no_oacr
-POPD
-
-IF %_BUILDARCH%==x86 SET Lib=%Lib%\Crt\i386;%DDK_LIB_DEST%\i386;%Lib%
-IF %_BUILDARCH%==AMD64 SET Lib=%Lib%\Crt\amd64;%DDK_LIB_DEST%\amd64;%Lib%
+echo Run this file from "VS20xx <arch> Native Tools Command Prompt"
 
 FOR /F %%I IN ('DIR /B /S src\zopfli\*.c') DO CALL :cl %%I
 CALL :link zopfli.exe
@@ -12,13 +7,13 @@ FOR %%I IN (
 zopfli_bin.obj
 ) DO IF EXIST %%I DEL /Q %%I
 
-FOR /F %%I IN ('DIR /B /S src\zopflipng\*.c*') DO CALL :cl /I%INCLUDE%\crt\stl60 /EHsc %%I
+FOR /F %%I IN ('DIR /B /S src\zopflipng\*.c*') DO CALL :cl /I"%INCLUDE%\crt\stl60" /EHsc %%I
 CALL :link zopflipng.exe
 
 GOTO :EOF
 
 :cl
-cl.exe -nologo -O1 /GL /GS- /I%CRT_INC_PATH% /I. -c /MD /DNDEBUG %*
+cl.exe -nologo -O1 /GL /GS- /I"%CRT_INC_PATH%" /I. -c /MD /DNDEBUG %*
 GOTO :EOF
 
 :link
